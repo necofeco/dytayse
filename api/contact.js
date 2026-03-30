2676222975a1b2216b497fb0a047c26655cb33b9
necofeco <necofeco004@gmail.com>
2026-03-31 02:00:16 +0300

    update
    update

--- api/contact.js
+++ api/contact.js
@@ -26,7 +26,7 @@ function requiredEnv(name) {
   return v;
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS (optional but helpful)
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
